846b5b31743fa56cd1ee2b958722196a54256e63
Cuile <i@cuile.com>
2020-07-12 12:05:34 +0800

    更新启动命令

--- docker/bash/nginx_down.sh
+++ docker/bash/nginx_down.sh
@@ -2,4 +2,4 @@
 
 docker-compose -f ../yml/nginx.yml down
 
-iptables -D INPUT -p tcp -m multiport --dport 80,25 -j ACCEPT
+iptables -D INPUT -p tcp -m multiport --dport 80,25,110 -j ACCEPT
--- docker/bash/nginx_up.sh
+++ docker/bash/nginx_up.sh
@@ -2,4 +2,4 @@
 
 docker-compose -f ../yml/nginx.yml up -d --build
 
-iptables -A INPUT -p tcp -m multiport --dport 80,25 -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --dport 80,25,110 -j ACCEPT
